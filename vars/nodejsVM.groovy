48f7d4f5e85cb31857af3c6073b4b8e58920f40a
Jayakrishna7478 <jayakrishnachowdry123@gmail.com>
2024-02-06 20:43:01 +0530

    catalogue

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -88,7 +88,7 @@ def call(Map configMap){
             stage('Deploy') {
                 when {
                     expression{
-                        params.Deploy
+                        params.Deploy == 'true'
                     }
                 }
                 steps {
