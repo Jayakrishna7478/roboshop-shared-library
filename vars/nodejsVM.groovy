6716452b525ebe4fe2547ba2af9008b9c3e9668e
Jayakrishna7478 <jayakrishnachowdry123@gmail.com>
2024-02-07 15:16:29 +0530

    chaged to sg vpc

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -8,7 +8,7 @@ def call(Map configMap){
         environment { 
             packageVersion = ''
             // can maintain in pipeline globals
-             nexusURL = '172.31.8.127:8081'
+            // nexusURL = '172.31.8.127:8081'
         }
         options {
             timeout(time: 1, unit: 'HOURS')
---
+++ vars/pipelineGlobals.groovy
@@ -0,0 +1,3 @@
+static final String nexusURL(){
+    return "172.31.8.127:8081"
+}
